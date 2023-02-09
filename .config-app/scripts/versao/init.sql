0a6b0225edabe7aa160816f2d0ad9718f3075f82
Ramon Oliveira <Ramon.Oliveira@mercanet.com.br>
2023-02-09 20:45:08 -0300

    ajuste escript

--- .config-app/scripts/versao/init.sql
+++ .config-app/scripts/versao/init.sql
@@ -16,7 +16,7 @@ create table agendamento  (
       REFERENCES cliente(id)
    );
 
-
+//cavalo infancia ramon senha
 INSERT INTO auth.users (instance_id,id,aud,"role",email,encrypted_password,email_confirmed_at,last_sign_in_at,raw_app_meta_data,raw_user_meta_data,is_super_admin,created_at,updated_at,phone,phone_confirmed_at,confirmation_token,email_change,email_change_token_new,recovery_token) VALUES
 	('00000000-0000-0000-0000-000000000000'::uuid,'420df6a5-8ae2-42ac-b39f-4ea992c031a2'::uuid,'authenticated','authenticated','luiscersar@gmail.com','$2a$10$E8SfKgmnt9Rawhs84j4yiuOZenz7mkbaANoflBBo8KHP5sEkhWKAu','2023-02-11 21:02:04.547','2023-02-11 22:53:12.520','{"provider": "email", "providers": ["email"]}','{}',FALSE,'2023-02-11 21:02:04.542','2023-02-11 21:02:04.542',NULL,NULL,'','','','')
 ON CONFLICT (id) DO NOTHING;
